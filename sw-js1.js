b355cf4e28df87f1b9266f81b628f0b7a6e9d8fc
sapirminster-cmd <sapirminster@gmail.com>
2026-03-04 16:25:44 +0200

    Add files via upload

---
+++ sw-js1.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'skin-ritual-v5';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/SkinRoutine/',
+        '/SkinRoutine/index.html',
+        '/SkinRoutine/manifest.json'
+      ]).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
